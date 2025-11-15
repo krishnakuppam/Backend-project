f10208996e81b8f61b509502373c83d81e0596f6
Krishna R A <krishnakuppam38@gmail.com>
2025-11-15 18:48:06 +0530

    nov 15

--- MongoDB/Post1.js
+++ MongoDB/Post1.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose');
 const app = express();
 const Product = require('./model/products');
 
+
+
+main().catch(console.error);
 app.use(express.json());
 
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://krishnakuppam38:krishnakuppam@cluster0.25wgae8.mongodb.net/test');
+        await mongoose.connect('mongodb+srv://krishna:krishna@cluster0.vkucudk.mongodb.net/');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
@@ -22,27 +25,12 @@ async function main() {
 
 
 
-app.get('/products', async (req, res) => {
-
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-
-})
-
-
-
-
-
 app.get('/', (req, res) => {
     res.send('Hello World!  MongoDB Connection Test');
 });
 
 
-main().catch(console.error);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
